94c5502bcb492311628c913f7ef1ae1c44a4b46b
zucrose <uzrhmdkhn@gmail.com>
2025-07-06 17:27:18 +0530

    Added Turnovers Year over Year

--- pspark/TurnoversYOY.py
+++ pspark/TurnoversYOY.py
@@ -5,6 +5,9 @@ from pyspark.sql.functions import col, lit, udf, broadcast
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 from rich.jupyter import display
 
+import matplotlib.pyplot as plt
+import pandas as pd
+
 spark = SparkSession.builder \
     .appName("LoadPlayerDim")\
     .config('spark.jars.packages', 'org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.8.1') \
@@ -17,21 +20,26 @@ spark = SparkSession.builder \
     .config("spark.sql.catalog.spark_catalog.type", "hive") \
     .getOrCreate()
 
+def load():
+    actionDF=spark.sql('Select * from local.db.dim_action')
+    factDF=spark.sql('Select * from local.db.fact_playByPlay')
+    gameLogDF=spark.sql('Select game_id as gameId,season_year from local.db.gamelogs')
+
+    actionFactDF=factDF.join(broadcast(actionDF),['actionkey'])
+    nDF=actionFactDF.filter(actionFactDF.actionType=='Turnover').join(broadcast(gameLogDF),'gameId').groupby('season_year','subtype').count()
+
+    nDF.writeTo('local.db.YOY_TO').createOrReplace()
 
 
 
 
-pbprawdf=spark.sql('Select * from local.db.playbyplay')
-actiondf=spark.sql('Select * from local.db.dim_action')
-
-pbpa=pbprawdf.join(broadcast(actiondf),['actionType','subtype','shotResult','isFieldGoal'])
-pbpa=pbpa.drop('actionType','subtype','playerName','playerNameI','teamTricode','shotResult','isFieldGoal')
-
-#pbprawdf.show()
-
-pbpa.writeTo('local.db.fact_playByPlay').createOrReplace()
-spark.sql('Select * from local.db.fact_playByPlay').show()
+subtypes=spark.sql('Select distinct subtype from local.db.yoy_to').collect()
+fig, ax = plt.subplots()
+for i in subtypes:
+     pandasDF=spark.sql(f"Select * from local.db.yoy_to where subtype='{i[0]}' order by 1 asc").toPandas()
+     #print(pandasDF)
+     pandasDF.plot(ax=ax,kind='line' , x='season_year',y='count',label=i[0])
 
 
 
-
+plt.savefig("plots/TO_YOY.png")
